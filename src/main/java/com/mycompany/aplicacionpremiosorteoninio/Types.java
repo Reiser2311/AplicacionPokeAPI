6bd29dc50b8d6cbec9b6b81aace6332bdc570405
Iván Cuesta <148111204+Reiser2311@users.noreply.github.com>
2024-01-24 08:49:35 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/aplicacionpremiosorteoninio/Types.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aplicacionpremiosorteoninio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Types {
+    private int slot;
+    private Type type;
+
+    public Types(int slot, Type type) {
+        this.slot = slot;
+        this.type = type;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public void setSlot(int slot) {
+        this.slot = slot;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+    
+    
+}
